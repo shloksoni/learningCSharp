a1a7f1c9da47999c23ca2e6ddb89fc8de62d9eca
Shlok Soni <s2shlok@gmail.com>
2021-02-24 23:05:20 +0530

    Added AsyncAwaitAssignment

---
+++ src/AsyncAwaitAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitAssignment
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ReadAndWriteAsync();
+        }
+        public static async Task ReadAndWriteAsync()
+        {
+            string ReadFilePath = "A.txt";
+            string text = await File.ReadAllTextAsync(ReadFilePath);
+            string WriteFilePath = "B.txt";
+            await File.WriteAllTextAsync(WriteFilePath, text);
+
+        }
+
+    }
+}
